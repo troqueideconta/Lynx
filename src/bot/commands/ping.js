f6e8a617315fae2d1e1775050613870ba7c5debb
pedrofurlanc <88358595+pedrofurlanc@users.noreply.github.com>
2021-10-11 23:32:15 -0300

    Update ping.js

--- src/bot/commands/ping.js
+++ src/bot/commands/ping.js
@@ -4,8 +4,8 @@ const Command = require('../../classes/Command');
 const command = new Command('ping', 'Informações');
 command.setAliases('ms')
 .setExecute(async execParams => {
-    const { message, client } = execParams;
-message.channel.send(`🏓${Math.round(client.ws.ping)}ms)
-  })
+  const {message, client, args} = execParams;
+message.channel.send(`🏓${Math.round(client.ws.ping)}ms`)
+});
 
   module.exports = command;
