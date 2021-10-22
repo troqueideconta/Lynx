2010e6a8635d0e183c803887e3e7434c39eb8ee8
pedrofurlanc <88358595+pedrofurlanc@users.noreply.github.com>
2021-10-22 00:19:22 -0300

    Update invite.js

--- src/bot/commands/invite.js
+++ src/bot/commands/invite.js
@@ -1,1 +1,45 @@
+const { MessageEmbed } = require('discord.js');
+const Command = require('../../classes/Command');
+const moment = require("moment");
+require("moment-duration-format");
+const { MessageButton } = require('discord-buttons');
 
+const command = new Command('invite', 'Informações');
+command.setAliases('convidar')
+.setExecute(async execParams => {
+        const {message, client, args} = execParams;
+
+  const Embed = new MessageEmbed() 
+    
+  .setTitle(`🤖 | Me convide para o seu servidor!`)
+  .setThumbnail(client.user.avatarURL({ dynamic: true }))
+  .setDescription(`**😸 |Me adicione**: [Clique aqui](https://discord.com/oauth2/authorize?client_id=781310999864148009&scope=bot&permissions=8)
+  **☎ |Meu servidor de suporte**: [Clique aqui](https://discord.gg/Gs65zux2Cr)`)
+  .setColor(client.config.embedMainColor)
+
+  let invite = new MessageButton()
+  .setStyle('url')
+  .setLabel('Invite') 
+  .setURL('https://discord.com/oauth2/authorize?client_id=781310999864148009&scope=bot&permissions=8')
+  .setDisabled(false); 
+
+  let suport = new MessageButton()
+  .setStyle('url')
+  .setLabel('Support') 
+  .setURL('https://discord.gg/Gs65zux2Cr')
+  .setDisabled(false); 
+
+  message.channel.send({
+      embed: Embed,
+      components: [
+        {
+          type: 1,
+          components: [invite, suport]
+        }
+      ]
+  })
+  
+  
+  })
+
+  module.exports = command;
