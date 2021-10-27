3e837ee2b231994d9615386b13734a377c4ad3c4
pedrofurlanc <88358595+pedrofurlanc@users.noreply.github.com>
2021-10-27 18:47:15 -0300

    Update unlock.js

--- src/bot/commands/unlock.js
+++ src/bot/commands/unlock.js
@@ -1,1 +1,28 @@
 
+const { Command } = require("../../classes/CommandHandler");
+const ms = require("ms");
+const { MessageEmbed } = require("discord.js");
+const { MessageButton } = require('discord-buttons');
+const moment = require('moment');
+moment.locale('pt-BR')
+
+const command = new Command('unlock', 'Staff')
+command.setAliases('desbloquear')
+.setExecute(async execParams => {
+    const {message, client, args} = execParams;
+    if (!message.member.permissions.has('MANAGE_SERVER', 'MANAGE_CHANNELS')) return message.channel.send("🚫 |Você não tem as permissões necessárias para usar esse comando.")
+        message.channel.overwritePermissions([
+         {
+            id: message.guild.id,
+            null : ['SEND_MESSAGES', 'ADD_REACTIONS'],
+         },
+        ],);
+        const embed = new MessageEmbed()
+        .setTitle("Canal atualizado!")
+        .setDescription(`🔓 ${message.channel}  foi desbloqueado`)
+        .setColor(client.config.embedMainColor)
+        await message.channel.send(embed);
+    })
+
+
+module.exports = command;
